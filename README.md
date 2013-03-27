d21ce04ee577938847e77da29d80c26b5e3c351e
Jason Knabl <jasonknabl@le5-18-68.resnet.carleton.ca>
2013-03-27 10:58:54 -0400

    first

---
+++ README.md
@@ -0,0 +1,13 @@
+Pyvideo Download
+
+Pull all videos from a given PyCon to your local box.
+
+To use:
+
+1. Set DL_PATH to point at an existing path on your machine. Alternatively, leave the field blank to download to whatever directory you place this script in.
+
+2. Head to pyvideo.org and pull up the files page for a particular PyCon. e.g.: http://pyvideo.org/category/33/pycon-us-2013/files
+
+3. Assign the relevant URL to PYVIDEO_URL.
+
+4. python pyvideo_dl.py
---
+++ pyvideo_dl.py
@@ -0,0 +1,51 @@
+import pprint
+import lxml.html
+import requests
+import os
+import re
+# Set the (EXISTING) path where you want the videos downloaded
+# e.g. /Users/me/Documents/videos/
+#
+# Leave blank to download to wherever you run this script from
+DL_PATH = "/Users/jasonknabl/Documents/pycon-canada-2012/"
+PYVIDEO_URL = "http://pyvideo.org/category/25/pycon-ca-2012/files"
+# Begin logic
+
+f = requests.get(PYVIDEO_URL)
+parsed = lxml.html.document_fromstring(f.content)
+XMLItems = parsed.cssselect("table.table td")
+item_list = {}
+#get links, make a dict.
+for td in XMLItems:
+    if td.cssselect("a"):
+        item_list[curr_title] = td.cssselect('a')[0].attrib['href']
+    else:
+        curr_title = re.sub(r'\W+', '', td.text_content())
+        curr_title = curr_title.encode('ascii', 'ignore')
+        item_list[curr_title] = ""
+#iterate through dict & pull video data
+for talk in item_list:
+    new_path = DL_PATH + talk
+    if os.path.exists("%s.mp4" % new_path):
+        print "Already downloaded %s.mp4. Skipping...\n---\n" % new_path
+        continue
+    print "Creating file: %s.mp4" % new_path
+    with open('%s.mp4' % new_path, 'wb') as handle:
+        print "File %s.mp4 created." % new_path
+        print "Downloading and writing to %s.mp4..." % talk
+        if item_list[talk] == "":
+            print "NO URL: can't download.\n---\n"
+            os.remove('%s.mp4' % new_path)
+            continue
+        try:
+            req = requests.get(item_list[talk], stream=True)
+        except OSError:
+            print "Invalid filepath. Try a new one.\n---\n"
+            continue
+        for block in req.iter_content(1024):
+            if not block:
+                break
+            handle.write(block)
+        print "Success!\n---\n"
+print "All done."
+
